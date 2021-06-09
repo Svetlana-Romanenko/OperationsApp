e0603f12f4331bd0f12613bcd42844fec9425fed
Svetlana <svetlana.romanenko@megafon.ru>
2021-06-09 18:23:52 +0300

    Initial Commit

---
+++ OperationsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OperationsApp
+//
+//  Created by Светлана Романенко on 09.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
